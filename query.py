d02b9c5da9697efee5f3c0085e1cbbcb48ba71a3
hedi-fss <moalla.hedi@fss.u-sfax.tn>
2025-07-29 12:23:43 +0100

    Names of divisions and services

--- query.py
+++ query.py
@@ -1,6 +1,6 @@
 import sqlite3
 con = sqlite3.connect("instance/users_data.db")
 cur=con.cursor()
-cur.execute("Update service set nom='Intelligence Artificielle' where nom='IA'")
+cur.execute("Update service set nom='Secrétariat' where Id_service=10")
 con.commit()
 con.close()
